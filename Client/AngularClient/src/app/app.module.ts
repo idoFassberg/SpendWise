d0cee4f269156eb888427f82429d43199ee01351
Ido Fassberg <idofassberg@gmail.com>
2024-01-19 23:02:53 +0200

    add left menu

--- Client/AngularClient/src/app/app.module.ts
+++ Client/AngularClient/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { LeftMenuComponent } from './left-menu/left-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    LeftMenuComponent,
   ],
   imports: [
     BrowserModule,
